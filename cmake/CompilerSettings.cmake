2fb535135d28ef1d287d6d51f71d391c6e339d3c
Eitan Stein <31282300+EitanStein@users.noreply.github.com>
2026-05-29 10:39:25 +0000

    remove MSVC related settings as this will run only on linux (or only POSIX)

--- cmake/CompilerSettings.cmake
+++ cmake/CompilerSettings.cmake
@@ -13,7 +13,6 @@ function(create_project_options)
     endif()
 
     # 3. Create readable aliases for complex logic
-    set(IS_MSVC  $<CXX_COMPILER_ID:MSVC>)
     set(IS_GCC   $<CXX_COMPILER_ID:GNU>)
     set(IS_CLANG $<CXX_COMPILER_ID:Clang>)
     set(IS_DEBUG $<CONFIG:Debug>)
@@ -26,17 +25,11 @@ function(create_project_options)
     # ---------------------------------------------------------
     if(TRACY_ENABLE)
         target_compile_options(project_options INTERFACE
-            # Windows Warnings: Level 4 and treat as errors
-            $<$<AND:${IS_MSVC}>:/W4>
-            
             # Linux Warnings: Wall, Wextra, Pedantic, and treat as errors
             $<$<AND:${IS_POSIX}>:-Wall -Wextra -Wpedantic>
         )
     else()
         target_compile_options(project_options INTERFACE
-            # Windows Warnings: Level 4 and treat as errors
-            $<$<AND:${IS_MSVC}>:/W4 /WX>
-            
             # Linux Warnings: Wall, Wextra, Pedantic, and treat as errors
             $<$<AND:${IS_POSIX}>:-Wall -Wextra -Wpedantic -Werror>
         )
@@ -46,15 +39,6 @@ function(create_project_options)
     # SANITIZER FLAGS (Only if enabled and in Debug mode)
     # ---------------------------------------------------------
     if(ENABLE_SANITIZERS)
-        
-        # --- MSVC (Windows) Sanitizers ---
-        target_compile_options(project_options INTERFACE
-            $<$<AND:${IS_MSVC},${IS_DEBUG}>:/fsanitize=address>
-        )
-        target_link_options(project_options INTERFACE
-            $<$<AND:${IS_MSVC},${IS_DEBUG}>:/fsanitize=address>
-        )
-
         # --- GCC/Clang (Linux) Sanitizers ---
         target_compile_options(project_options INTERFACE
             $<$<AND:${IS_POSIX},${IS_DEBUG}>:-fsanitize=address,undefined -fno-omit-frame-pointer>
