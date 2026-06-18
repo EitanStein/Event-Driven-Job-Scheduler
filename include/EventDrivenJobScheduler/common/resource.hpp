61488b6cda8f36dba60778cfc6834f4a04d58ded
Eitan Stein <31282300+EitanStein@users.noreply.github.com>
2026-06-18 20:22:53 +0000

    add const to comparison Resource methods

--- include/EventDrivenJobScheduler/common/resource.hpp
+++ include/EventDrivenJobScheduler/common/resource.hpp
@@ -6,16 +6,16 @@ struct Resource {
     size_t memory;
     size_t cpu;
 
-    [[nodiscard]] constexpr bool operator>(const Resource& other){
+    [[nodiscard]] constexpr bool operator>(const Resource& other) const{
         return (memory > other.memory && cpu > other.cpu);
     }
-    [[nodiscard]] constexpr bool operator>=(const Resource& other){
+    [[nodiscard]] constexpr bool operator>=(const Resource& other) const{
         return (memory >= other.memory && cpu >= other.cpu);
     }
-    [[nodiscard]] constexpr bool operator<(const Resource& other){
+    [[nodiscard]] constexpr bool operator<(const Resource& other) const{
         return (memory < other.memory && cpu < other.cpu);
     }
-    [[nodiscard]] constexpr bool operator<=(const Resource& other){
+    [[nodiscard]] constexpr bool operator<=(const Resource& other) const{
         return (memory <= other.memory && cpu <= other.cpu);
     }
 
