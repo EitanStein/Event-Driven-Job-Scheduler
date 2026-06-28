90e9b04422c8b341a834902cc5e86132c4aeb5bb
Eitan Stein <31282300+EitanStein@users.noreply.github.com>
2026-06-28 21:07:59 +0000

    add test case with client communication

--- tests/manager/test_manager.cpp
+++ tests/manager/test_manager.cpp
@@ -4,6 +4,7 @@
 #include <catch2/catch_test_macros.hpp>
 #include "EventDrivenJobScheduler/utils/log_macros.hpp"
 #include "EventDrivenJobScheduler/manager/manager.hpp"
+#include "EventDrivenJobScheduler/client/client.hpp"
 
 
 TEST_CASE("Empty job queue", ""){
@@ -44,4 +45,34 @@ TEST_CASE("multiple jobs with final invalid job", "")
     REQUIRE(manager.giveJobToWorker());
     REQUIRE(manager.giveJobToWorker());
     REQUIRE(manager.giveJobToWorker());
+}
+
+
+TEST_CASE("managing job from client", "")
+{
+    INIT_LOGGER();
+    Manager manager;
+    int pid = fork();
+
+    if(pid > 0){
+        Client client;
+        sleep(1);
+        REQUIRE(client.connectToManager() == CommStatus::Success);
+        REQUIRE(client.sendCommand(MsgFormat{"--exec sleep --args 1"}) == CommStatus::Success);
+        REQUIRE(client.sendCommand(MsgFormat{"--exec echo --args 1"}) == CommStatus::Success);
+        _exit(0);
+    }
+    else if(pid == 0){
+        REQUIRE(manager.handleSignal() == SignalHandler::Signal::Client);
+        REQUIRE(manager.handleSignal() == SignalHandler::Signal::Ignore);
+        SignalHandler::Signal result = manager.handleSignal();
+        REQUIRE((result == SignalHandler::Signal::Ignore || result == SignalHandler::Signal::Worker));
+        result = manager.handleSignal();
+        REQUIRE((result == SignalHandler::Signal::Ignore || result == SignalHandler::Signal::Worker));
+        result = manager.handleSignal();
+        REQUIRE((result == SignalHandler::Signal::Ignore || result == SignalHandler::Signal::Worker));
+        result = manager.handleSignal();
+        REQUIRE((result == SignalHandler::Signal::Ignore || result == SignalHandler::Signal::Worker));
+    }
+
 }
